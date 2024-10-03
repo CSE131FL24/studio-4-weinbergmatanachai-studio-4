6f6673c810e7644c2ea25ce81d5417f68d383ca2
ken.matanachai <ken.matanachai@localhost>
2024-10-03 10:39:00 -0500

    Studio 4

--- src/studio4/NorwayMotherOfAllFlags.java
+++ src/studio4/NorwayMotherOfAllFlags.java
@@ -7,96 +7,119 @@ import edu.princeton.cs.introcs.StdDraw;
 import support.cse131.DialogBoxes;
 
 /**
- * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
+ * @author Dennis Cosgrove
  */
 public class NorwayMotherOfAllFlags {
-	private static void mother() {
-		StdDraw.clear();
-		StdDraw.setPenColor(255, 0, 0);
-		StdDraw.filledRectangle(3, 3, 3, 3);
-		StdDraw.filledRectangle(3, 13, 3, 3);
-		StdDraw.filledRectangle(16, 3, 6, 3);
-		StdDraw.filledRectangle(16, 13, 6, 3);
-		StdDraw.setPenColor(0, 0, 127);
-		StdDraw.filledRectangle(11, 8, 11, 1);
-		StdDraw.filledRectangle(8, 8, 1, 8);
-	}
 
-	public static void norway() {
-		StdDraw.setXscale(0, 22);
-		StdDraw.setYscale(0, 16);
-		mother();
-	}
+    private static void mother() {
+        StdDraw.clear();
+        StdDraw.setPenColor(255, 0, 0); 
+        StdDraw.filledRectangle(3, 3, 3, 3); 
+        StdDraw.filledRectangle(3, 13, 3, 3);
+        StdDraw.filledRectangle(16, 3, 6, 3);
+        StdDraw.filledRectangle(16, 13, 6, 3);
+        StdDraw.setPenColor(0, 0, 127); 
+        StdDraw.filledRectangle(11, 8, 11, 1); 
+        StdDraw.filledRectangle(8, 8, 1, 8); 
+    }
 
-	public static void france() {
-		
-		// TODO
-	}
+    public static void norway() {
+        StdDraw.setXscale(0, 22);
+        StdDraw.setYscale(0, 16);
+        mother();
+    }
 
-	public static void indonesia() {
-		
-		// TODO
-	}
+    public static void france() {
+        StdDraw.clear();
+        StdDraw.setPenColor(0, 0, 255);
+        StdDraw.filledRectangle(2.5, 8, 2.5, 8); 
+        StdDraw.setPenColor(255, 255, 255); 
+        StdDraw.filledRectangle(11, 8, 4.5, 8);
+        StdDraw.setPenColor(255, 0, 0);
+        StdDraw.filledRectangle(19.5, 8, 2.5, 8); 
+    }
 
-	public static void netherlands() {
-		
-		// TODO
-	}
+    public static void indonesia() {
+        StdDraw.clear();
+        StdDraw.setPenColor(255, 0, 0); 
+        StdDraw.filledRectangle(11, 12, 11, 4); 
+        StdDraw.setPenColor(255, 255, 255);
+        StdDraw.filledRectangle(11, 4, 11, 4); 
+    }
 
-	public static void poland() {
-		
-		// TODO
-	}
+    public static void netherlands() {
+        StdDraw.clear();
+        StdDraw.setPenColor(255, 0, 0); 
+        StdDraw.filledRectangle(11, 12, 11, 4);
+        StdDraw.setPenColor(255, 255, 255); 
+        StdDraw.filledRectangle(11, 8, 11, 4); 
+        StdDraw.setPenColor(0, 0, 255); 
+        StdDraw.filledRectangle(11, 4, 11, 4); 
+    }
 
-	public static void thailand() {
-		
-		// TODO
-	}
+    public static void poland() {
+        StdDraw.clear();
+        StdDraw.setPenColor(255, 255, 255); 
+        StdDraw.filledRectangle(11, 12, 11, 4); 
+        StdDraw.setPenColor(255, 0, 0);
+        StdDraw.filledRectangle(11, 4, 11, 4); 
+    }
 
-	public static void finland() {
-		
-		// TODO
-	}
+    public static void thailand() {
+        StdDraw.clear();
+        StdDraw.setPenColor(255, 0, 0); 
+        StdDraw.filledRectangle(11, 14, 11, 2); 
+        StdDraw.filledRectangle(11, 2, 11, 2);
+        StdDraw.setPenColor(0, 0, 255);
+        StdDraw.filledRectangle(11, 8, 11, 3); 
+        StdDraw.setPenColor(255, 255, 255); 
+        StdDraw.filledRectangle(11, 11, 11, 1);
+        StdDraw.filledRectangle(11, 5, 11, 1); 
+    }
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			int canvasHeight = 512;
-			int canvasWidth = 22 * canvasHeight / 16;
-			StdDraw.setCanvasSize(canvasWidth, canvasHeight);
-			Object[] options = { "Continue", "Exit" };
-			while (true) {
-				norway();
-				if (DialogBoxes.askUser("Continue to Flag of France?", "Current: Norway", JOptionPane.QUESTION_MESSAGE,
-						options)) {
-					france();
-					if (DialogBoxes.askUser("Continue to Flag of Indonesia?", "Current: France",
-							JOptionPane.QUESTION_MESSAGE, options)) {
-						indonesia();
-						if (DialogBoxes.askUser("Continue to Flag of Netherlands?", "Current: Indonesia",
-								JOptionPane.QUESTION_MESSAGE, options)) {
-							netherlands();
-							if (DialogBoxes.askUser("Continue to Flag of Poland?", "Current: Netherlands",
-									JOptionPane.QUESTION_MESSAGE, options)) {
-								poland();
-								if (DialogBoxes.askUser("Continue to Flag of Thailand?", "Current: Poland",
-										JOptionPane.QUESTION_MESSAGE, options)) {
-									thailand();
-									if (DialogBoxes.askUser("Continue to Flag of Finland?", "Current: Thailand",
-											JOptionPane.QUESTION_MESSAGE, options)) {
-										finland();
-										if (DialogBoxes.askUser("Exit?", "Current: Finland")) {
-											// pass
-										} else {
-											continue;
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-				System.exit(0);
-			}
-		});
-	}
+    public static void finland() {
+        StdDraw.clear();
+        StdDraw.setPenColor(255, 255, 255); 
+        StdDraw.filledRectangle(11, 8, 11, 8); 
+        StdDraw.setPenColor(0, 0, 255); 
+        StdDraw.filledRectangle(6, 8, 2, 8);
+        StdDraw.filledRectangle(11, 5.5, 11, 2); 
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            int canvasHeight = 512;
+            int canvasWidth = 22 * canvasHeight / 16;
+            StdDraw.setCanvasSize(canvasWidth, canvasHeight);
+            Object[] options = { "Continue", "Exit" };
+            while (true) {
+                norway();
+                if (DialogBoxes.askUser("Continue to Flag of France?", "Current: Norway", JOptionPane.QUESTION_MESSAGE, options)) {
+                    france();
+                    if (DialogBoxes.askUser("Continue to Flag of Indonesia?", "Current: France", JOptionPane.QUESTION_MESSAGE, options)) {
+                        indonesia();
+                        if (DialogBoxes.askUser("Continue to Flag of Netherlands?", "Current: Indonesia", JOptionPane.QUESTION_MESSAGE, options)) {
+                            netherlands();
+                            if (DialogBoxes.askUser("Continue to Flag of Poland?", "Current: Netherlands", JOptionPane.QUESTION_MESSAGE, options)) {
+                                poland();
+                                if (DialogBoxes.askUser("Continue to Flag of Thailand?", "Current: Poland", JOptionPane.QUESTION_MESSAGE, options)) {
+                                    thailand();
+                                    if (DialogBoxes.askUser("Continue to Flag of Finland?", "Current: Thailand", JOptionPane.QUESTION_MESSAGE, options)) {
+                                        finland();
+                                        if (DialogBoxes.askUser("Exit?", "Current: Finland")) {
+                                            
+                                            System.exit(0);
+                                        } else {
+                                            continue;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                System.exit(0);
+            }
+        });
+    }
 }
