c8f361ef3c4b1c300818be5c364bbd49ad35224d
ken.matanachai <ken.matanachai@localhost>
2024-10-03 10:32:07 -0500

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,15 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setXscale(0,30);
+		StdDraw.setYscale(0,18);
+		
+		StdDraw.clear();
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledRectangle(6,3,30,3);
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledRectangle(6, 9, 30,3);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledRectangle(6, 15, 30,3);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,24 +1,52 @@
 package studio4;
-
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 import javax.swing.JFileChooser;
-import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
 
-/**
- * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
- */
 public class InterpretDrawingFile {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		JFileChooser chooser = new JFileChooser("resources");
-		chooser.showOpenDialog(null);
-		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+    
+        JFileChooser chooser = new JFileChooser("resources");
+        chooser.showOpenDialog(null);
+        File f = new File(chooser.getSelectedFile().getPath());
+
+        Scanner in = new Scanner(f);
+        
+    
+        String shapeType = in.next();
+        
+
+        int red = in.nextInt();
+        int green = in.nextInt();
+        int blue = in.nextInt();
+        
+
+        boolean isFilled = in.nextBoolean();
+        
+
+        StdDraw.setPenColor(red, green, blue);
+        
+      
+        if (shapeType.equals("rectangle")) {
+           
+            double x = in.nextDouble();
+            double y = in.nextDouble();
+            double halfWidth = in.nextDouble();
+            double halfHeight = in.nextDouble();
+            
+            
+            if (isFilled) {
+                StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+            } else {
+                StdDraw.rectangle(x, y, halfWidth, halfHeight);
+            }
+        }
+      
+        in.close();
+    }
 }
